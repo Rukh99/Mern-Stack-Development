aa90d21d24d280ed442acf76408c071d00547946
Rukh99 <rukhsarqureshi469@gmail.com>
2023-08-22 13:01:55 +0530

    javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,69 @@
-console.log('hello javascropt');
+console.log('hello javascript');
+
+console.log('nice');
+
+myname = "RUKHSAR";
+
+console.log(myname);
+
+num = (36532);
+
+console.log(num);
+console.log(typeof num );
+
+console.log(typeof myname);
+
+isloggedin = true;
+
+console.log(typeof isloggedin);
+
+age = 23;
+
+if(age>= 18)
+{
+    console.log('eligible for DL');
+}
+else{
+    console.log('not eligible for DL');
+}
+
+process.stdout.write('same line 1');
+process.stdout.write('same line 2\n');
+
+//write a program to check is divisible by 7 and 11
+
+n1 = 777;
+if (n1%7 == 0  && n1%11 == 0)
+{
+    console.log('DIVISIBLE BY 7 as well as 11');
+}
+else if (n1%7 == 0)
+{
+    console.log('DIVISIBLE ONLY BY 7 ');
+}
+else if (n1%11==0)
+{
+    console.log('Divisible only 11');
+}
+else{
+    console.log ('Not Divisible by 7 or 11');
+}
+
+console.log(3456 === 3456);
+
+console.log('3456' === 3456);
+
+console.log(3456 == 3456);
+
+console.log (56 + 23 + '23');
+
+console.log (4 ** 2);
+
+console.log(25 ** 0.5);
+
+
+
+
+
+
+
